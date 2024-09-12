62e1101671475ef3e9ab756acfd06e9252377841
Cephas Tay <cephas.tay137@gmail.com>
2024-09-12 11:26:07 +0000

    Changed the syntax for calculating the area

--- python_introduction/rectangle_area.py
+++ python_introduction/rectangle_area.py
@@ -3,7 +3,7 @@
 length = 10
 width = 5
 
-area = length*width
+area = length * width
 
 print('Assuming a rectangle has dimensions of; Width ' +str(width)+ ' and Length ' +str(length)+ ' in metres')
 print('The area of the rectangle is: '+str(area))
