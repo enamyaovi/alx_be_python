05d114e5df95886b62ad7038d8cebac4524e98be
Cephas Yaovi Tay <cephas.tay137@gmail.com>
2024-09-20 19:02:02 +0000

    Update weather_advice.py to include appropriate variable name
    The checker was actually flagging my variable name. I used user_input instead of weather. So hopefully here is our final commit for task 1.

--- control-flow/weather_advice.py
+++ control-flow/weather_advice.py
@@ -1,11 +1,11 @@
 # This is a program written in python that gives weather advice to users
 
-user_input = input("What's the weather like today? (sunny/rainy/cold): ").lower()
-if user_input == "sunny":
+weather = input("What's the weather like today? (sunny/rainy/cold): ").lower()
+if weather == "sunny":
     print("Wear a t-shirt and sunglasses.")
-elif user_input == "rainy":
+elif weather == "rainy":
     print("Don't forget your umbrella and raincoat.")
-elif user_input == "cold":
+elif weather == "cold":
     print("Make sure to wear a warm coat and scarf.")
 else:
     print("Sorry, I don't have recommendations for this weather")
