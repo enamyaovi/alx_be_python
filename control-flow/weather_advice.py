9b8f329c2b90e162a1680023b43341e4e346dbbe
Cephas Yaovi Tay <cephas.tay137@gmail.com>
2024-09-20 18:58:02 +0000

    Update weather_advice.py
    The checker was flagging my f literal syntax and \ to escape. So I changed everything to reflect that

--- control-flow/weather_advice.py
+++ control-flow/weather_advice.py
@@ -1,12 +1,12 @@
 # This is a program written in python that gives weather advice to users
 
 user_input = input("What's the weather like today? (sunny/rainy/cold): ").lower()
-if user_input == 'sunny':
-    print(f'Wear a t-shirt and sunglasses.')
-elif user_input == 'rainy':
-    print(f'Don\'t forget your umbrella and raincoat.')
-elif user_input == 'cold':
-    print(f'Make sure to wear a warm coat and scarf.')
+if user_input == "sunny":
+    print("Wear a t-shirt and sunglasses.")
+elif user_input == "rainy":
+    print("Don't forget your umbrella and raincoat.")
+elif user_input == "cold":
+    print("Make sure to wear a warm coat and scarf.")
 else:
-    print(f'Sorry, I don\'t have recommendations for this weather')
+    print("Sorry, I don't have recommendations for this weather")
 
