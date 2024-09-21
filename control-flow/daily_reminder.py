e45d285c01c3c190b4dd5842c025a83f40697fe3
Cephas Tay <cephas.tay137@gmail.com>
2024-09-21 12:42:24 +0000

    Reverted back to old code for daily_reminder.py script as new format failed checker

--- control-flow/daily_reminder.py
+++ control-flow/daily_reminder.py
@@ -6,33 +6,22 @@ task = input('Enter your task: ').strip().capitalize()
 priority = input('Priority (high/medium/low): ').lower().strip()
 time_bound = input('Is it time-bound? (yes/no):').lower().strip()
 
-#variables for creating custom statements
-message = ''
-custom_message = ''
-
 #match and case block for priority level nested with if statements for task urgency
-#just trying something over here
 match priority:
     case 'high':
         if time_bound == 'yes':
-            message += f'Reminder:'
-            custom_message += f' that requires immediate attention today!'
+            print(f'Reminder: \'{task}\' is a high priority task that requires immediate attention today!')
         else:
-            message += f'Note:'
-            custom_message += f'. Consider completing it when you have free time.'
+            print(f'Reminder: \'{task}\' is a high priority task. Try to get to it as soon as you can!')
+
     case 'medium':
         if time_bound == 'yes':
-            message += f'Reminder:'
-            custom_message += f' that requires immediate attention today!'
+            print(f'Note: \'{task}\' is a medium priority task that requires attention today!')
         else:
-            message += f'Note:'
-            custom_message += f'. Consider completing it when you have free time.'
+            print(f'Note: \'{task}\' is a medium priority task.')
+
     case 'low':
         if time_bound == 'yes':
-            message += f'Reminder:'
-            custom_message += f' that requires immediate attention today!'
+            print(f'Note: \'{task}\' is a low priority task that requires attention today!')
         else:
-            message += f'Note:'
-            custom_message += f'. Consider completing it when you have free time.'
-
-print(f"{message} '{task}' is a {priority} priority task{custom_message}") # prints custom message calling on various variables 
+            print(f'Note: \'{task}\' is a low priority task. Consider completing it when you have free time.')
