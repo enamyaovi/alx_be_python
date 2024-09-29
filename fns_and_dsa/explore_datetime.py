403fe7412f1d7c0e96cab80c8ce4e2da432bfd31
Cephas Tay <cephas.tay137@gmail.com>
2024-09-29 19:04:05 +0000

    Updated explore datetime module

--- fns_and_dsa/explore_datetime.py
+++ fns_and_dsa/explore_datetime.py
@@ -1,6 +1,6 @@
 # This project is about experimenting with the datetime module in python
 
-import datetime
+from datetime import datetime, timedelta
 
 
 
@@ -8,13 +8,13 @@ import datetime
 def display_current_datetime():
     """Understanding the Python DateTime Module"""
 
-    current_date = datetime.datetime.now()
+    current_date = datetime.now()
     parsed_date = current_date.strftime('%Y-%m-%d %H:%M:%S')
     print(f'Current date and time: {parsed_date}')
 
     # prompting for user input needed for calculating future date
     number_of_days = int(input('Enter the number of days to add to the current date: '))
-    time_delta = datetime.timedelta(days = number_of_days)
+    time_delta = timedelta(days = number_of_days)
 
     def calculate_future_date():
         """Calculates the future date by user input"""
