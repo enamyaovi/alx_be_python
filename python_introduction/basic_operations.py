249874922bd57bb49cda0f2051e2c4b7584c4bd8
Cephas Tay <cephas.tay137@gmail.com>
2024-10-06 14:30:51 +0000

    Rewrote Basic Operations as a function

--- python_introduction/basic_operations.py
+++ python_introduction/basic_operations.py
@@ -5,12 +5,24 @@ number1 = 10
 number2 = 5
 
 # Here are some basic operations performed on the two variables
-sum_of_numbers = number1 + number2
-diff_of_numbers = number1 -number2
-prod_of_numbers = number2*number1
+def operations(number1:int, number2:int):
+    """"This function returns the sum, difference and product of 
+    two predefined integers (10 and 5)"""
+
+    sum_of_numbers = number1 + number2
+    diff_of_numbers = number1 - number2
+    prod_of_numbers = number2 * number1
+    return sum_of_numbers, diff_of_numbers, prod_of_numbers
 
 #onto the print commands
-print('assuming the number1 = ' +str(number1)+ ' and the number2= '+str(number2))
-print('Addition of ' +str(number1) +' and ' +str(number2) +' is ' + str(sum_of_numbers))
-print('Subtraction of ' +str(number1) +' and ' +str(number2) +' is ' + str(diff_of_numbers))
-print('Multiplication of ' +str(number1) +' and ' +str(number2) +' is ' + str(prod_of_numbers))
+def main():
+
+    sum_of_numbers, diff_of_numbers, prod_of_numbers = operations(number1, number2)
+
+    print('assuming the number1 = ' +str(number1)+ ' and the number2= '+str(number2))
+    print('Addition of ' +str(number1) +' and ' +str(number2) +' is ' + str(sum_of_numbers))
+    print('Subtraction between ' +str(number1) +' and ' +str(number2) +' is ' + str(diff_of_numbers))
+    print('Multiplication of ' +str(number1) +' and ' +str(number2) +' is ' + str(prod_of_numbers))
+
+if __name__ == '__main__':
+    main()
