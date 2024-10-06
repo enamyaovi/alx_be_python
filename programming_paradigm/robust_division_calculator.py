8f09dd849f3a58ed307986ac1260bc55785df31a
Cephas Yaovi Tay <cephas.tay137@gmail.com>
2024-10-06 18:44:14 +0000

    error message correction

--- programming_paradigm/robust_division_calculator.py
+++ programming_paradigm/robust_division_calculator.py
@@ -6,7 +6,7 @@ def safe_divide(numerator: int, denominator:int):
         result = numerator/denominator
         return f'The result of the division is {result:.1f}'
     except ZeroDivisionError:
-        return f'Error cannot divide by zero.'
+        return f'Error: Cannot divide by zero.'
     except ValueError:
         return f'Error: Please enter numeric values only.'
 
